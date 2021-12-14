d19a2d9991d2da5dd3a93ea707afb04c9a1fa498
Geoffrey Lin <glin20@stuy.edu>
2021-12-14 00:41:22 -0500

    edited things

--- pipe_networking.c
+++ pipe_networking.c
@@ -11,18 +11,27 @@
   =========================*/
 int server_handshake(int *to_client) {
   mkfifo(WKP, 0644);
+  printf("Reading from client...\n");
   int from_client = open(WKP, O_RDONLY);
   char received[100] = {'\0'};
   int i = read(from_client, received, sizeof(received));
   if (i){
     printf("Receive success: %s\n", received);
   }
+  else {
+    printf("Error: %s\n", strerror(errno));
+  }
   remove(WKP);
+  printf("Sending to client...\n");
   *to_client = open(received, O_WRONLY);
   write(*to_client, ACK, strlen(ACK));
+  printf("Reading from client again...\n");
   i = read(from_client, received, sizeof(received));
   if (i){
     printf("Received message: %s\n", received);
+  }
+  else {
+    printf("Error: %s\n", strerror(errno));
   }
   return from_client;
 }
@@ -45,13 +54,19 @@ int client_handshake(int *to_server) {
   mkfifo(pid, 0644);
   printf("PID: %d\n", pidNum);
   *to_server = open(WKP, O_WRONLY);
+  printf("Writing to server...\n");
   write(*to_server, pid, strlen(pid));
+  printf("Reading from server...\n");
   int from_server = open(pid, O_RDONLY);
   int i = read(from_server, message, sizeof(message));
   if (i){
-    printf("Received messaged: %s\n", message);
+    printf("Received message: %s\n", message);
+  }
+  else {
+    printf("Error: %s\n", strerror(errno));
   }
   remove(pid);
+  printf("Writing to server again...\n");
   write(*to_server, ACK, strlen(ACK));
   return from_server;
 }
